905c3c93d09257c5113047ac595a523e8e3f273d
Constantin ARMAND <constantin.armand@gmail.com>
2021-12-15 14:21:51 +0100

    Up Régles

---
+++ ts/index.ts
@@ -0,0 +1,3 @@
+//SQUID GAME
+//Règlements :
+// 2 joueur , chaque joueur posséde un sac avec 10 billes , avant de débuter chaque joueur prend un nombre de billes  entre 1 et 10. Ensuite un challenger doit deviner si le nombre de billes dans la main de son adversaire est pair ou impair , si il a raison alors il prend le même nombre de bille qu'il a dans la main a son adversaire . Si il perd l'adversaire prend l'équivalent du nombre qu'il a dans la main au challenger.
