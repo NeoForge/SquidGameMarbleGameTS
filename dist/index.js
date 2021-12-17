08572713413e958a93d3a71ace4aadd29e16ef0a
aminebneji <aminebneji@gmail.com>
2021-12-17 09:20:46 +0100

    Amine
    Amine 17/12/2021

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,107 @@
 "use strict";
 //SQUID GAME
 //Règlements :
-// 2 joueur , chaque joueur posséde un sac avec 10 billes , avant de débuter chaque joueur prend un nombre de billes  entre 1 et 10. Ensuite un challenger doit deviner si le nombre de billes dans la main de son adversaire est pair ou impair , si il a raison alors il prend le même nombre de bille qu'il a dans la main a son adversaire . Si il perd l'adversaire prend l'équivalent du nombre qu'il a dans la main au challenger.
+// 2 joueur , chaque joueur posséde un sac avec 10 billes , avant de débuter chaque joueur prend un nombre de billes  entre 1 et 10. Ensuite un challenger doit deviner si le nombre de billes dans la main de son adversaire est pair ou il prend le même nombre de bille qu'impair , si il a raison alors il a dans la main a son adversaire . Si il perd l'adversaire prend l'équivalent du nombre qu'il a dans la main au challenger.
+class joueur {
+    constructor(name, marbleInHand, marbleBag) {
+        this.name = name;
+        this.marbleInHand = marbleInHand;
+        this.marbleBag = marbleBag;
+    }
+}
+let challengeur = new joueur("chall", 0, 10);
+let ordinateur = new joueur("adv", 0, 10);
+let aQuiLeTour = true;
+function choixDuNombreDeBilles() {
+    if (challengeur.marbleBag < 20) {
+        let nb;
+        nb = prompt("combien tu mets frero", "1");
+        if (nb == challengeur.marbleBag) {
+            challengeur.marbleInHand = parseInt(nb);
+            console.log("t'as toutes tes billes dans la main POGGER");
+        }
+        else if (nb > challengeur.marbleBag) {
+            choixDuNombreDeBilles();
+            return console.log("t'as les yeux plus gros que le ventre");
+        }
+        else if (nb < 0 || nb == 0) {
+            console.log("il faut parier quelquechose ... ");
+            choixDuNombreDeBilles();
+            return;
+        }
+        else {
+            challengeur.marbleInHand = parseInt(nb);
+        }
+    }
+    if (ordinateur.marbleBag === 20) {
+        console.log("c'est rapé frero t'es perdu à tout jamais");
+    }
+    console.log("t'as " + challengeur.marbleInHand + " billes dans la main");
+    return;
+}
+function ordinateurBillesSetup() {
+    ordinateur.marbleInHand = Math.floor(Math.random() * ordinateur.marbleBag) + 1;
+    console.log("ton adversaire à  " + ordinateur.marbleInHand + " billes dans la main");
+    return;
+}
+function reponseNombreDeBilles() {
+    let reponseChallengeur;
+    reponseChallengeur = prompt("Tu pense qu'il a un nombre de billes paire ou impaire dans la main ?");
+    if (ordinateur.marbleInHand % 2 == 0 && reponseChallengeur == "paire") {
+        challengeur.marbleBag += challengeur.marbleInHand;
+        ordinateur.marbleBag -= challengeur.marbleInHand;
+        aQuiLeTour = false;
+        return console.log(" ggwp !");
+    }
+    else if (ordinateur.marbleInHand % 2 != 0 && reponseChallengeur == "impaire") {
+        challengeur.marbleBag += challengeur.marbleInHand;
+        ordinateur.marbleBag -= challengeur.marbleInHand;
+        aQuiLeTour = false;
+        return console.log(" ggwp !");
+    }
+    else {
+        challengeur.marbleBag -= ordinateur.marbleInHand;
+        ordinateur.marbleBag += ordinateur.marbleInHand;
+        aQuiLeTour = false;
+        return console.log("t'as perdu");
+    }
+}
+function ordinateurReponseNombreDeBilles() {
+    let reponseOrdinateur = Math.floor(Math.random() * 2);
+    if (challengeur.marbleInHand % 2 == 0 && reponseOrdinateur == 0) {
+        ordinateur.marbleBag += ordinateur.marbleInHand;
+        challengeur.marbleBag -= ordinateur.marbleInHand;
+        aQuiLeTour = true;
+        return console.log(" ggwp !");
+    }
+    else if (challengeur.marbleInHand % 2 != 0 && reponseOrdinateur == 1) {
+        ordinateur.marbleBag += ordinateur.marbleInHand;
+        challengeur.marbleBag -= ordinateur.marbleInHand;
+        aQuiLeTour = true;
+        return console.log(" ggwp !");
+    }
+    else {
+        challengeur.marbleBag += challengeur.marbleInHand;
+        ordinateur.marbleBag -= challengeur.marbleInHand;
+        aQuiLeTour = true;
+        return console.log("t'as perdu");
+    }
+}
+function gameLoop() {
+    choixDuNombreDeBilles();
+    ordinateurBillesSetup();
+    if (aQuiLeTour) {
+        reponseNombreDeBilles();
+    }
+    else if (!aQuiLeTour) {
+        ordinateurReponseNombreDeBilles();
+    }
+    challengeur.marbleInHand = 0;
+    ordinateur.marbleInHand = 0;
+    console.log(challengeur);
+    console.log(ordinateur);
+    if (challengeur.marbleBag == 20 || ordinateur.marbleBag == 20) {
+        return console.log("c'est fini pour aujourd'hui");
+    }
+}
+gameLoop();
--- ts/index2.ts
+++ ts/index2.ts
@@ -1,86 +1,86 @@
-//SQUID GAME
-//Règlements :
-// 2 joueur , chaque joueur posséde un sac avec 10 billes , avant de débuter chaque joueur prend un nombre de billes  entre 1 et 10. Ensuite un challenger doit deviner si le nombre de billes dans la main de son adversaire est pair ou impair , si il a raison alors il prend le même nombre de bille qu'il a dans la main a son adversaire . Si il perd l'adversaire prend l'équivalent du nombre qu'il a dans la main au challenger.
+// //SQUID GAME
+// //Règlements :
+// // 2 joueur , chaque joueur posséde un sac avec 10 billes , avant de débuter chaque joueur prend un nombre de billes  entre 1 et 10. Ensuite un challenger doit deviner si le nombre de billes dans la main de son adversaire est pair ou impair , si il a raison alors il prend le même nombre de bille qu'il a dans la main a son adversaire . Si il perd l'adversaire prend l'équivalent du nombre qu'il a dans la main au challenger.
 
-class Player 
-{
-    name : string;
-    hand : number;
-    marbleInBag : number;
-    constructor(name : string, hand : number, marbleInBag : number)
-    {
-        this.name = name;
-        this.hand = hand;
-        this.marbleInBag = marbleInBag;
-    }
+// class Player 
+// {
+//     name : string;
+//     hand : number;
+//     marbleInBag : number;
+//     constructor(name : string, hand : number, marbleInBag : number)
+//     {
+//         this.name = name;
+//         this.hand = hand;
+//         this.marbleInBag = marbleInBag;
+//     }
 
-    ChoseNumberOfMarbleInHand = (nb : number) =>
-    {
-        if(nb > this.marbleInBag)
-        {
-            this.hand = nb;
-            return nb;
-        }
-        else
-        {
-            return console.log("Vous n'avez pas assez de billes dans votre sac");
-        }
-    }
+//     ChoseNumberOfMarbleInHand = (nb : number) =>
+//     {
+//         if(nb > this.marbleInBag)
+//         {
+//             this.hand = nb;
+//             return nb;
+//         }
+//         else
+//         {
+//             return console.log("Vous n'avez pas assez de billes dans votre sac");
+//         }
+//     }
 
-    CheckDeath = () =>
-    {
-        if(this.marbleInBag <= 0)
-        {
-            return console.log("Vous êtes mort");
-        }
-        else
-        {
-            return console.log("Vous êtes vivant");
-        }
-    }
-}
+//     CheckDeath = () =>
+//     {
+//         if(this.marbleInBag <= 0)
+//         {
+//             return console.log("Vous êtes mort");
+//         }
+//         else
+//         {
+//             return console.log("Vous êtes vivant");
+//         }
+//     }
+// }
 
-function TransferMarble(Giver:Player,Receiver:Player)
-{
-    if(Giver.marbleInBag < Receiver.hand)
-    {
-        Receiver.marbleInBag += Giver.marbleInBag;
-        Giver.marbleInBag = 0;
-        Giver.CheckDeath();
-    }
-    else
-    {
-        Receiver.marbleInBag += Receiver.hand;
-        Giver.marbleInBag -= Receiver.hand;
-        Giver.CheckDeath();
-    }
-} 
-function CompareMarbleInHand(Challenger:Player,Adversaire:Player,choice : string)
-{
-    if(choice =="pair")
-    {
-        if(Adversaire.hand % 2 == 0)
-        {
-            TransferMarble(Challenger,Adversaire);
-            return console.log("Vous avez gagné");
-        }
-        else
-        {
-            TransferMarble(Adversaire,Challenger);
-            return console.log("Vous avez perdu");
-        }
-    }
-    else
-    {
-        if(Adversaire.hand % 2 != 0)
-        {
-            TransferMarble(Challenger,Adversaire);
-            return console.log("Vous avez gagné");
-        }
-        else
-        {
-            TransferMarble(Adversaire,Challenger);
-            return console.log("Vous avez perdu");
-        }
-    }
-}
+// function TransferMarble(Giver:Player,Receiver:Player)
+// {
+//     if(Giver.marbleInBag < Receiver.hand)
+//     {
+//         Receiver.marbleInBag += Giver.marbleInBag;
+//         Giver.marbleInBag = 0;
+//         Giver.CheckDeath();
+//     }
+//     else
+//     {
+//         Receiver.marbleInBag += Receiver.hand;
+//         Giver.marbleInBag -= Receiver.hand;
+//         Giver.CheckDeath();
+//     }
+// } 
+// function CompareMarbleInHand(Challenger:Player,Adversaire:Player,choice : string)
+// {
+//     if(choice =="pair")
+//     {
+//         if(Adversaire.hand % 2 == 0)
+//         {
+//             TransferMarble(Challenger,Adversaire);
+//             return console.log("Vous avez gagné");
+//         }
+//         else
+//         {
+//             TransferMarble(Adversaire,Challenger);
+//             return console.log("Vous avez perdu");
+//         }
+//     }
+//     else
+//     {
+//         if(Adversaire.hand % 2 != 0)
+//         {
+//             TransferMarble(Challenger,Adversaire);
+//             return console.log("Vous avez gagné");
+//         }
+//         else
+//         {
+//             TransferMarble(Adversaire,Challenger);
+//             return console.log("Vous avez perdu");
+//         }
+//     }
+// }
